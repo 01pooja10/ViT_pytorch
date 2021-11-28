c13a4195b5d64097a37d9561c571261fbcfe693d
Pooja Ravi <kuchan2001@gmail.com>
2021-11-28 10:53:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from src.emb import Embed
+from src.embedding import Embed
 from src.attn import Attention
 from src.blocks import Blocks
+from src.mlp import MLP
 from src.vit import ViT
 
 def check():
