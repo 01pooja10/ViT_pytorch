9064fd9c3e324f388272e330780591d9a09ff430
Pooja Ravi <kuchan2001@gmail.com>
2021-11-30 10:47:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
-from src.embedding import Embed
-from src.attn import Attention
-from src.blocks import Blocks
-from src.mlp import MLP
+import torch
+import torch.nn as nn
+import numpy and np
 from src.vit import ViT
+import warnings
+warnings.filter_warnings('ignore')
 
-def check():
-	
 def test():
+    mod = ViT().cuda()
+	mod = mod.eval()
+    inp = torch.randn((2, 3, 384, 384)).cuda()  #send model and inputs to GPU
+    out = mod(inp)
+    print(out.shape)
+    print(out.mean())
+    print('Tensor: ',out)
+
+if __name__ == '__main__':
+	test()
