18b0a3b2ff95fbcb9942ce5be7cc59a2256df015
Pooja Ravi <kuchan2001@gmail.com>
2021-11-30 11:05:49 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -7,10 +7,27 @@ import argparse
 from src.vit import ViT
 
 parser = argparse.ArgumentParser()
-parser.add_argument("Image Size", help = "Argument to determine image size")
-parser.add_argument("Patch Size", help = "Argument to determine patch size")
-parser.add_argument("Transformer Depth", help = "Argument to determine number of attention blocks")
-parser.add_argument("Output Dropout Probability", help = "Argument to control dropout probability of output")
-parser.add_argument("Attention Dropout Probability", help = "Argument to control dropout probability of attention layer")
+
+parser.add_argument("depth", help = "Integer to determine number of attention blocks")
+parser.add_argument("projp", help = "Float value to control dropout probability of output")
+parser.add_argument("attnp", help = "Float value to control dropout probability of attention layer")
+parser.add_argument("gpu", help = "Boolean to confirm presence of GPU")
+parser.add_argument("wts", help = "Path to weights of the trained model")
+
 arg = parser.parse_args()
+
+model = ViT(depth=args.depth, projp=args.projp, attnp=args.attnp)
+inp = torch.randn((16, 3, 384, 384))
+
+if args.gpu:    
+    model = model.cuda()
+    inp = inp.cuda()
+
+model.load_state_dict(torch.load(args.wts))
+model.eval()
+
+with torch.no_grad():
+  out = model(inp)
+ 
+
 
